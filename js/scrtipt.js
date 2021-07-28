861abdc3904c3b0c4c9f7261d2e9a20379ff0afa
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-07-28 14:08:16 +0200

    aggiunte comandi a js

---
+++ js/scrtipt.js
@@ -0,0 +1,8 @@
+// chiedo nome
+var nome = prompt("Qual'é il tuo Nome ?");
+// chiedo cognome
+var cognome = prompt("Qual'é il tuo Cognome ?");
+// chiedo colore pref
+var color = prompt("Qual'é il tuo colore preferito?");
+// mostro messaggio
+document.getElementById('password').innerHTML += nome + cognome + color + "40";
